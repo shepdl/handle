450cd66c9c5c066a9823b82e911719db2151cdb8
Dave Shepard <dave@humnet.ucla.edu>
2019-06-13 12:00:39 -0700

    Remove incorrect dependency

--- src/main/java/edu/ucla/drc/sledge/DocumentImport.java
+++ src/main/java/edu/ucla/drc/sledge/DocumentImport.java
@@ -1,18 +1,16 @@
 package edu.ucla.drc.sledge;
 
-import com.sun.tools.internal.ws.wsdl.document.Import;
 import edu.ucla.drc.sledge.documentimport.*;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
