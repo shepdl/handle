9d917fa20dc1d32ba0e3cbbef14f6d15ff4a0099
Dave Shepard (CDH) <dave@humnet.ucla.edu>
2019-09-11 07:59:44 -0700

    Move export buttons and add handlers

--- src/main/java/edu/ucla/drc/sledge/topicmodeling/ModelVisualization.java
+++ src/main/java/edu/ucla/drc/sledge/topicmodeling/ModelVisualization.java
@@ -1,4 +1,33 @@
 package edu.ucla.drc.sledge.topicmodeling;
 
+import edu.ucla.drc.sledge.topicmodel.TopicModel;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.util.List;
+import java.util.TreeSet;
+
 public class ModelVisualization {
+    private TopicModel model;
+
+    public void exportModel(MouseEvent mouseEvent) {
+        // TODO: write them to the database
+    }
+
+    public void exportTopics(MouseEvent mouseEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select output");
+        File file = fileChooser.showSaveDialog();
+        if (file != null) {
+            // Draw image
+            List<TreeSet> set = model.getSortedWords();
+            for (int i = 0; i < model.getNumTopics(); i++) {
+            }
+        }
+    }
+
+    public void setData (TopicModel model) {
+        this.model = model;
+    }
 }
